532f505aab5e60c47becf1287cc495832fcdc798
Nyasha2 <nyasha.m@usapschool.org>
2024-07-17 09:42:42 -0700

    initial commit

---
+++ audio_smoothener/src/cuda_header.cuh
@@ -0,0 +1,21 @@
+/* 
+ * CUDA cuda_header
+ */
+
+#ifndef CUDA_HEADER_CUH_
+#define CUDA_HEADER_CUH_
+
+#ifdef __CUDA_ARCH__
+
+// Device function attributes
+#include <cuda_runtime.h>
+#define CUDA_CALLABLE __host__ __device__
+
+#else
+
+// Host function attributes
+#define CUDA_CALLABLE
+
+#endif // __CUDA_ARCH__
+
+#endif // CUDA_HEADER_CUH_
